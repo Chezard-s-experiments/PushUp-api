117e6c53dd4933a036c9b40996dfd7dedccc4084
matias <matias.taillade@gmail.com>
2026-03-16 13:07:21 +0100

    :sparkles: OAuthProvider

--- src/core/users/value_objects/__init__.py
+++ src/core/users/value_objects/__init__.py
@@ -1,4 +1,5 @@
 from src.core.users.value_objects.email import Email
 from src.core.users.value_objects.hashed_password import HashedPassword
+from src.core.users.value_objects.oauth_provider import OAuthProvider
 
-__all__ = ["Email", "HashedPassword"]
+__all__ = ["Email", "HashedPassword", "OAuthProvider"]
